47821188607b0b06e28646e4210bf64bc0f07088
JX <jxie1995@gmail.com>
2020-06-04 18:52:27 -0700

    add login, register routes and ejs layout/views and connect database

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
 
+// Database
+mongoose.connect(process.env.DATABASEURI, { useNewUrlParser: true, useUnifiedTopology: true }, err => {
+    if (!err) { console.log('Connected to DataBase') }
+});
+
+
+// EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
 
 // Routes
 app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
 
 
 const PORT = process.env.PORT || 5000;
