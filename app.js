fed4b996c650d42d268316b9002d43cfb46af7e2
JX <jxie1995@gmail.com>
2020-06-04 22:59:06 -0700

    add validation for registering, password hashing, store new user in db and messages (validation messages and successful register flash messages)

--- app.js
+++ app.js
@@ -1,19 +1,50 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 require('dotenv').config();
 
 const app = express();
 
 // Database
-mongoose.connect(process.env.DATABASEURI, { useNewUrlParser: true, useUnifiedTopology: true }, err => {
-    if (!err) { console.log('Connected to DataBase') }
-});
-
+mongoose.connect(process.env.DATABASEURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to Database'))
+    .catch(err => console.log(err));
 
 // EJS
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
+
+// Bodyparser
+app.use(express.urlencoded({ extended: true })); //bodyparser is now part of express
+
+// Express Session Middleware
+app.use(session({
+    secret: process.env.SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 1000 * 60
+    }
+}))
+
+// Session Store
+const store = new MongoDBStore({
+    uri: process.env.DATABASEURI,
+    collection: 'sessions'
+})
+
+// Connect Flash Middleware
+app.use(flash());
+
+// Custom Flash Middleware
+app.use((req, res, next) => {
+    res.locals.success_message = req.flash('success_msg');
+    res.locals.error_message = req.flash('error_msg');
+    next();
+})
 
 // Routes
 app.use('/', require('./routes/index'));
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,76 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 
+// User Model
+const User = require('../models/User');
+
+// Login Page
 router.get('/login', (req, res) => {
     res.render('login');
 })
 
+// Register Page
 router.get('/register', (req, res) => {
     res.render('register');
+})
+
+// Register Handler
+router.post('/register', (req, res) => {
+    const { name, email, password, password2 } = req.body;
+    let errors = [];
+
+    // Check required fields
+    if (!name || !email || !password || !password2) {
+        errors.push({ msg: 'Please fill in all fields' });
+    }
+
+    // Check password match
+    if (password != password2) {
+        errors.push({ msg: 'Passwords do not match' })
+    }
+
+    // Check pass length
+    if (password.length < 8) {
+        errors.push({ msg: 'Password should be at least 8 characters' })
+    }
+
+    if (errors.length > 0) {
+        res.render('register', {
+            errors: errors,
+            name: name,
+            email: email,
+            password: password,
+            password2: password2
+        })
+    } else {
+        // Validation passed
+        User.findOne({ email: email }, (err, doc) => {
+
+            if (doc) {
+                errors.push({ msg: 'User already registered. Please log in or try a different email'})
+                res.render('register', {
+                    errors,
+                    name,
+                    email,
+                    password,
+                    password2
+                })
+            } else {
+                bcrypt.hash(password, 15, (err, hash) => {
+                    let user = new User({
+                        name,
+                        email,
+                        password: hash
+                    });
+                    user.save();
+                })
+                req.flash('success_msg', 'Registration Successful. You can now log in');
+                res.redirect('/users/login');
+            }
+
+        })
+    }
 })
 
 module.exports = router;
